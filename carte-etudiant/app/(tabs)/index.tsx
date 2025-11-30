bf4a6a586aeae2e03facf3a74a7084286e0ad602
Salma Foulah <s.foulah7@gmail.com>
2025-11-30 18:41:13 +0100

    Ajout du dossier carte-etudiant

---
+++ carte-etudiant/app/(tabs)/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react'; 
+import { Image, StyleSheet, Text, View } from 'react-native'; 
+ 
+export default function App() { 
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.container}>
+        <Image 
+          source={require('../../assets/emsi.png')} 
+          style={styles.logo} 
+          resizeMode="contain" 
+        /> 
+        <Text style={styles.schoolName}>EMSI MOULAY YOUSSEF</Text>
+      </View>
+
+      <View style={styles.info}> 
+        <Text style={styles.label}>Nom : <Text style={styles.value}>FOULAH</Text></Text> 
+        <Text style={styles.label}>Prénom : <Text style={styles.value}>SALMA</Text></Text> 
+        <Text style={styles.label}>Année universitaire : <Text style={styles.value}>2025 / 2026</Text></Text> 
+      </View>
+    </View>
+
+  ); 
+} 
+ 
+const styles = StyleSheet.create({ 
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f6f7fb',
+  },
+  container: {               
+    justifyContent: 'center',  
+    alignItems: 'center',      
+    backgroundColor: '#f6f7fb', 
+    marginBottom: 20,
+    flexDirection: 'column',      
+  }, 
+  logo: { 
+    width: 200, 
+    height: 200, 
+  }, 
+  schoolName: { 
+    fontSize: 22, 
+    fontWeight: 'bold', 
+    color: '#1f2937', 
+    marginTop: 20,       
+    letterSpacing: 1, 
+  }, 
+  info: { 
+    alignItems: 'center', 
+    gap: 8,  
+  }, 
+  label: { 
+    fontSize: 16, 
+    color: '#6b7280', 
+    fontWeight: '500',
+    marginBottom: 8, 
+  }, 
+  value: { 
+    fontSize: 17, 
+    color: '#111827', 
+    fontWeight: 'bold', 
+  }, 
+}); 
