a3fbf9ce734d7e3a297c8283db41de7d1cc25ff8
salma-foulah <s.foulah7@gmail.com>
2025-11-30 15:12:43 +0100

    Ajout de la fonction addition dans index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('Hello Node');
+// index.js (branche dev) 
+function addition(a, b) { 
+return a + b; 
+} 
+if (require.main === module) { 
+console.log("Résultat:", addition(5, 3)); 
+} 
+module.exports = { addition };
