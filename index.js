ca9f23f7649e855df4f42084670f338107d5e9dc
salma-foulah <s.foulah7@gmail.com>
2025-11-30 15:16:27 +0100

    Ajout de la fonction soustraction

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ if (require.main === module) {
 console.log("Résultat:", addition(5, 3)); 
 } 
 module.exports = { addition };
+// index.js (branche feature) 
+function addition(a, b) { 
+return a + b; 
+} 
+function soustraction(a, b) { 
+return a - b; 
+} 
+if (require.main === module) { 
+console.log("Résultat addition:", addition(5, 3)); 
+console.log("Résultat soustraction:", soustraction(5, 3)); 
+} 
+module.exports = { addition, soustraction }; 
